76e0d4ac76d79ad4ff6ea379eb56c5f5596a5a0f
Pim Snel <post@pimsnel.com>
2023-01-23 14:15:21 +0100

    fix problems found in review

--- src/prefs.js
+++ src/prefs.js
@@ -52,117 +52,113 @@ function cssHexString(css) {
     return rrggbb;
 }
 
-if(shellVersion >= 40){
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
+const UI = Me.imports.ui;
+const _settings = ExtensionUtils.getSettings();
 
-  const ExtensionUtils = imports.misc.extensionUtils;
-  const Me = ExtensionUtils.getCurrentExtension();
-  const UI = Me.imports.ui;
-  const _settings = ExtensionUtils.getSettings();
-
-  /**
-   * Initialises the preferences widget
-   */
-  /* exported init */
-  function init() {
-    ExtensionUtils.initTranslations();
-  }
-
-  /**
-   * Builds the preferences widget
-   */
-  /* exported buildPrefsWidget */
-  function buildPrefsWidget() {
-
-    let widget = new HighlightFocusPrefsWidget();
-    return widget;
-  }
-
-
-  /**
-   * Describes the widget that is shown in the extension settings section of
-   * GNOME tweek.
-   */
-  const HighlightFocusPrefsWidget = new GObject.Class({
-    Name: 'Shortcuts.Prefs.Widget',
-    GTypeName: 'HighlightFocusPrefsWidget',
-    Extends: Gtk.ScrolledWindow,
-
-    /**
-     * Initalises the widget
-     */
-    _init: function() {
-      this.parent(
-        {
-          valign: Gtk.Align.FILL,
-          vexpand: true
-        }
-      );
-
-      this.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC);
-
-      this._grid = new UI.ListGrid();
-
-      this.set_child(new UI.Frame(this._grid));
-
-      let mainSettingsLabel = new UI.LargeLabel("Main Settings");
-      this._grid._add(mainSettingsLabel)
-
-
-      this._borderColorButton = new Gtk.ColorButton;
-      this._borderColorButton.set_valign("center");
-      this._borderColorButton.set_hexpand(false);
-
-      let label_border_color = new UI.Label('Border Color')
-      this._grid._add(label_border_color, this._borderColorButton);
-
-      _settings.bind("border-color", this._borderColorButton, "value", Gio.SettingsBindFlags.DEFAULT);
-
-      let rgba = new Gdk.RGBA();
-      rgba.parse(_settings.get_string('border-color'));
-      this._borderColorButton.set_rgba(rgba);
-
-      this._borderColorButton.connect('color-set', (button) => {
-        let rgba = button.get_rgba();
-        let css = rgba.to_string();
-        let hexString = cssHexString(css);
-        log("hallo?");
-        log(css);
-        log("hallo?1");
-        log(hexString);
-        _settings.set_string('border-color', hexString);
-      });
-
-
-      this._spin = new Gtk.SpinButton;
-      this._spin.set_range(0, 10);
-      this._spin.set_increments(1, 1);
-
-
-      let label_border_width = new UI.Label('Border Width')
-      this._grid._add(label_border_width, this._spin);
-      _settings.bind("border-width", this._spin, "value", Gio.SettingsBindFlags.DEFAULT);
-
-      this._spinRad = new Gtk.SpinButton;
-      this._spinRad.set_range(0, 20);
-      this._spinRad.set_increments(1, 1);
-
-      let label_border_rad = new UI.Label('Border Radius')
-      this._grid._add(label_border_rad, this._spinRad);
-      _settings.bind("border-radius", this._spinRad, "value", Gio.SettingsBindFlags.DEFAULT);
-
-      this._spinDelay = new Gtk.SpinButton;
-      this._spinDelay.set_range(0, 10000);
-      this._spinDelay.set_increments(100, 100);
-
-      let label_delay = new UI.Label('Hide delay in microseconds')
-      this._grid._add(label_delay, this._spinDelay);
-
-      _settings.bind("hide-delay", this._spinDelay, "value", Gio.SettingsBindFlags.DEFAULT);
-
-      let disableHiding = new UI.Check("Disable border hiding");
-      _settings.bind('disable-hiding', disableHiding, 'active', Gio.SettingsBindFlags.DEFAULT);
-      this._grid._add(disableHiding);
-
-    }
-  });
+/**
+ * Initialises the preferences widget
+ */
+/* exported init */
+function init() {
 }
+
+/**
+ * Builds the preferences widget
+ */
+/* exported buildPrefsWidget */
+function buildPrefsWidget() {
+
+  let widget = new HighlightFocusPrefsWidget();
+  return widget;
+}
+
+
+/**
+ * Describes the widget that is shown in the extension settings section of
+ * GNOME tweek.
+ */
+const HighlightFocusPrefsWidget = new GObject.Class({
+  Name: 'Shortcuts.Prefs.Widget',
+  GTypeName: 'HighlightFocusPrefsWidget',
+  Extends: Gtk.ScrolledWindow,
+
+  /**
+   * Initalises the widget
+   */
+  _init: function() {
+    this.parent(
+      {
+        valign: Gtk.Align.FILL,
+        vexpand: true
+      }
+    );
+
+    this.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC);
+
+    this._grid = new UI.ListGrid();
+
+    this.set_child(new UI.Frame(this._grid));
+
+    let mainSettingsLabel = new UI.LargeLabel("Main Settings");
+    this._grid._add(mainSettingsLabel)
+
+
+    this._borderColorButton = new Gtk.ColorButton;
+    this._borderColorButton.set_valign("center");
+    this._borderColorButton.set_hexpand(false);
+
+    let label_border_color = new UI.Label('Border Color')
+    this._grid._add(label_border_color, this._borderColorButton);
+
+    _settings.bind("border-color", this._borderColorButton, "value", Gio.SettingsBindFlags.DEFAULT);
+
+    let rgba = new Gdk.RGBA();
+    rgba.parse(_settings.get_string('border-color'));
+    this._borderColorButton.set_rgba(rgba);
+
+    this._borderColorButton.connect('color-set', (button) => {
+      let rgba = button.get_rgba();
+      let css = rgba.to_string();
+      let hexString = cssHexString(css);
+      log("hallo?");
+      log(css);
+      log("hallo?1");
+      log(hexString);
+      _settings.set_string('border-color', hexString);
+    });
+
+
+    this._spin = new Gtk.SpinButton;
+    this._spin.set_range(0, 10);
+    this._spin.set_increments(1, 1);
+
+
+    let label_border_width = new UI.Label('Border Width')
+    this._grid._add(label_border_width, this._spin);
+    _settings.bind("border-width", this._spin, "value", Gio.SettingsBindFlags.DEFAULT);
+
+    this._spinRad = new Gtk.SpinButton;
+    this._spinRad.set_range(0, 20);
+    this._spinRad.set_increments(1, 1);
+
+    let label_border_rad = new UI.Label('Border Radius')
+    this._grid._add(label_border_rad, this._spinRad);
+    _settings.bind("border-radius", this._spinRad, "value", Gio.SettingsBindFlags.DEFAULT);
+
+    this._spinDelay = new Gtk.SpinButton;
+    this._spinDelay.set_range(0, 10000);
+    this._spinDelay.set_increments(100, 100);
+
+    let label_delay = new UI.Label('Hide delay in microseconds')
+    this._grid._add(label_delay, this._spinDelay);
+
+    _settings.bind("hide-delay", this._spinDelay, "value", Gio.SettingsBindFlags.DEFAULT);
+
+    let disableHiding = new UI.Check("Disable border hiding");
+    _settings.bind('disable-hiding', disableHiding, 'active', Gio.SettingsBindFlags.DEFAULT);
+    this._grid._add(disableHiding);
+
+  }
+});
